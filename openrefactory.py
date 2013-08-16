63e21d27efda4938ebb10aaa84e016420cd44df5
Reed Allman <rdallman10@gmail.com>
2013-08-16 18:21:38 -0500

    tada, input params

--- openrefactory.py
+++ openrefactory.py
@@ -1,5 +1,8 @@
 import sublime_plugin, sublime
+import time
+import datetime
 from OpenRefactory import or_commands
+
  
 class RefactoryCommand(sublime_plugin.TextCommand):
   def run(self, edit):
@@ -14,27 +17,31 @@ class RefactoryCommand(sublime_plugin.TextCommand):
 
 
   def input_transformation(self):
-    self.transformations = or_commands.list(self.view)['transformations']
-    display = []
-    for t in self.transformations:
-      temp = []
-      for value in t.values():
-        temp.append(value)
-      display.append(temp)
-    self.view.window().show_quick_panel(display, self.on_input_transformation)
-
-
-  def on_input_transformation(self, index):
-    transformation = self.transformations[index]['shortName']
-    self.input_params(transformation)
+    print("input transformmation")
+    transformations = or_commands.list(self.view)['transformations']
+    self.view.window().show_quick_panel(
+      [[t['shortName'], t['name']] for t in transformations],
+      lambda i: self.input_params(transformations[i]['shortName']) if i > -1 else None)
 
   def input_params(self, transformation):
-    params = or_commands.params(self.view, transformation)['params']
-    self.new_params = []
-    for p in params:
-      self.view.window().show_input_panel(p['label'], str(p['default']), 
-        self.on_input_params, None, None)
+    def on_input_params(param):
+      new_params.append(param)
+      if params:
+        get_new_param(params.pop())
+      else:
+        self.validate_params(transformation, new_params)
 
-  def on_input_params(self, param):
-    self.new_params.append(param)
+    def get_new_param(param):
+      self.view.window().show_input_panel(param['label'], str(param['default']), 
+        on_input_params, None, None)
+
+
+    print("input params")
+    new_params = []
+    params = or_commands.params(self.view, transformation)['params']
+    get_new_param(params.pop())
+
+  def validate_params(self, transformation, params):
     pass
+
+
