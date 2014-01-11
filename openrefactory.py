175a4fd157d155ac09796295fbd1877fd2794e28
Reed Allman <rdallman10@gmail.com>
2014-01-11 16:25:16 -0600

    updated with names that work, readme

---
+++ openrefactory.py
@@ -0,0 +1,172 @@
+ # Contains all of the methods from the OpenRefactory Protocol.
+ # Each method will get a reply from the jar file and return if valid (if applicable)
+
+ # Copyright (c) 2013 Auburn University and others.
+ # All rights reserved. This program and the accompanying materials
+ # are made available under the terms of the Eclipse Public License v1.0
+ # which accompanies this distribution, and is available at
+ # http://www.eclipse.org/legal/epl-v10.html
+
+ # Contributors:
+ #    Reed Allman (Auburn) - Initial API and implementation
+
+import sublime_plugin, sublime
+import time
+import sys
+import difflib
+import subprocess
+import os
+from openrefactory_subl import or_commands
+
+# go to command palette, apply changes
+# show changed files in file palette
+
+# these are bad, and I should feel bad
+log = []
+files = []
+
+ 
+class BeginRefactorCommand(sublime_plugin.TextCommand):
+  def run(self, edit):
+    # list
+    # params
+    # validate
+    # xrun
+    ## log
+    ## files
+
+    self.input_transformation()
+
+
+  def input_transformation(self):
+    transformations = or_commands.list(self.view)['transformations']
+    self.view.window().show_quick_panel(
+      [[t['shortName'], t['name']] for t in transformations],
+      lambda i: self.input_params(transformations[i]['shortName']) if i > -1 else None)
+
+  def input_params(self, transformation):
+    def on_input_params(param):
+      new_params.append(param)
+      if params:
+        get_new_param(params.pop(0))
+      else:
+        self.validate_params(transformation, new_params)
+
+    def get_new_param(param):
+      self.view.window().show_input_panel(param['label'], str(param['default']), 
+        on_input_params, None, None)
+
+    new_params = []
+    params = or_commands.params(self.view, transformation)['params']
+    get_new_param(params.pop(0))
+
+  def validate_params(self, transformation, params):
+    result = or_commands.validate(self.view, transformation, params)['result']
+    for r in result:
+      if r['valid'] == 'false':
+        sublime.error_message(r['message']) 
+        sys.exit(0)
+    self.run_transformation(transformation, params)
+
+  def run_transformation(self, transformation, params):
+    response = or_commands.xrun(self.view, transformation, params)
+    global log, files
+    # yeah, I know....
+    log = response['log']
+    files = response['files']
+    if len(log):
+      content = "There were "+str(len(log))+" errors\n"
+      content += "Enter 'View Log' into command palette to view\n"
+      content += "Or Enter 'Execute Transformation' to continue"
+      self.view.window().run_command("show_console", {"content": content})
+    else:
+      content = "There were "+str(len(files))+" changed\n"
+      content += "Enter 'View Changes' into command palette to view\n"
+      content += "Or Enter 'Execute Transformation' to continue"
+      self.view.window().run_command("show_console", {"content": content})
+
+# TODO stick the dialog stuff in console w/ instructions --per zack  
+class ViewLogCommand(sublime_plugin.TextCommand):
+  def run(self, edit):
+    self.view_log()
+
+  def view_log(self):
+    self.view.window().show_quick_panel(
+      [l['context']['filename'] for l in log],
+      lambda i: self.get_diff(log[i]['context']))
+
+  # FUCK IT
+  # TODO quit fucking it
+  def get_diff(self, f):
+    # Reed, this is ugly
+    # better way to patch?
+    zFile = f['filename']
+    patchFile = f['patchFile']
+    with open(zFile, "r") as cfile:
+      from_content = cfile.readlines()
+    with open(patchFile, "r") as pfile:
+      to_content = pfile.readlines()
+    diffs = list(difflib.unified_diff(from_content, to_content))
+    diffs = map(lambda line: (line and line[-1] == "\n") and line or line + "\n", diffs)
+    diffs = ''.join(diffs)
+    scratch = self.view.window().new_file()
+    scratch.set_scratch(True)
+    scratch.set_syntax_file('Packages/Diff/Diff.tmLanguage')
+    scratch.run_command('file_diff', {'content': diffs})
+
+class ViewFilesCommand(sublime_plugin.TextCommand):
+  def run(self, edit):
+    self.view_files()
+    print(files)
+
+  def view_files(self):
+    if len(files):
+      self.view.window().show_quick_panel(
+        [f['filename'] for f in files],
+        lambda i: self.get_diff(files[i]))
+
+  # TODO FUCKING LEARN PYTHON MAN
+  def get_diff(self, f):
+    # Reed, this is ugly
+    # better way to patch?
+    zFile = f['filename']
+    patchFile = f['patchFile']
+    with open(zFile, "r") as cfile:
+      from_content = cfile.readlines()
+    with open(patchFile, "r") as pfile:
+      to_content = pfile.readlines()
+    diffs = list(difflib.unified_diff(from_content, to_content))
+    diffs = map(lambda line: (line and line[-1] == "\n") and line or line + "\n", diffs)
+    diffs = ''.join(diffs)
+    scratch = self.view.window().new_file()
+    scratch.set_scratch(True)
+    scratch.set_syntax_file('Packages/Diff/Diff.tmLanguage')
+    scratch.run_command('file_diff', {'content': diffs})
+
+class TransformCommand(sublime_plugin.TextCommand):
+  def run(self, edit):
+    self.patch_files()
+    self.view.run_command('revert')
+
+  def patch_files(self):
+    # TODO clean up the damn patch files and log
+    for f in files:
+      command = [
+        'patch', 
+        f["filename"],
+        f["patchFile"]
+      ]
+      subprocess.Popen(command, stdout=subprocess.PIPE, stderr=None)
+
+class FileDiffCommand(sublime_plugin.TextCommand):
+  def run(self, edit, content): 
+    self.view.insert(edit, 0, content)
+
+class ShowConsoleCommand(sublime_plugin.WindowCommand):
+  def run(self, content):
+    self.output_view = self.window.get_output_panel("textarea")
+
+    self.output_view.set_read_only(False)
+    self.output_view.run_command('file_diff', {'content': content})
+    self.output_view.set_read_only(True)
+    self.window.run_command("show_panel", {"panel": "output.textarea"})
---
+++ or_commands.py
@@ -0,0 +1,146 @@
+ # Contains all of the methods from the OpenRefactory Protocol.
+ # Each method will get a reply from the jar file and return if valid (if applicable)
+
+ # Copyright (c) 2013 Auburn University and others.
+ # All rights reserved. This program and the accompanying materials
+ # are made available under the terms of the Eclipse Public License v1.0
+ # which accompanies this distribution, and is available at
+ # http://www.eclipse.org/legal/epl-v10.html
+
+ # Contributors:
+ #    Reed Allman (Auburn) - Initial API and implementation
+
+import subprocess
+import json
+import os
+import sublime
+
+# multiple selections
+# think about view
+# can we eliminate set dir?
+def __send_command(command, view = None):
+  if view is not None:
+    command = ( '[' + __set_dir_auto(view) + ',' + command + ']' )
+  jar_command = [
+    'java', 
+    '-cp',
+    sublime.packages_path()+"/openrefactory_subl/ordemo.jar",
+    'org.openrefactory.internal.daemon.ORProxy',
+    command ]
+  print(jar_command)
+  proc = subprocess.Popen(jar_command, stdout=subprocess.PIPE, stderr=None)
+  # python sucks
+  response = proc.communicate()[0].strip().decode("utf-8")
+  print(response)
+  return response
+
+def __get_valid_response(command, view = None):
+  output = __send_command(command, view)
+  response = json.loads(output)
+
+  if response["reply"] == "OK":
+    return response
+  else:
+    raise Exception(response["message"])
+
+def __set_dir_auto(view):
+  # python sucks
+  dir = os.path.realpath(os.path.dirname(view.file_name()))
+  return __make_command('setdir', { 
+    'directory' : dir,
+    'mode'      : 'local' 
+    })
+
+def __get_text_selection(view):
+  # only supports first selection
+  sel = view.sel()[0]
+  return { 
+    'filename': view.file_name(),
+    'offset'  :   sel.begin(),
+    'length'  :   sel.size() }
+
+def __make_command(name, params = { }):
+  params['command'] = name
+  return json.dumps(params)
+
+def open():
+  command = __make_command('open', { 'version': 1.0 })
+  return __get_valid_response(command)
+
+def close():
+  command = __make_command('close')
+  __send_command(command)
+
+def about():
+  command = __make_command('about')
+  return __get_valid_response(command)
+
+def setdir(view):
+  dir = os.path.realpath(os.path.dirname(view.file_name()))
+  command = __make_command('setdir', { 
+    'diretory': dir,
+    'mode'    : 'local' 
+    })
+  return __get_valid_response(command, view)
+
+# returns
+#   reply: "OK",
+#   transformations: [{
+#     shortName, 
+#     name }, ... ]
+def list(view):
+  command = __make_command('list', {
+    'quality'       : "in_testing",
+    'textselection' : __get_text_selection(view) 
+    })
+  return __get_valid_response(command, view)
+
+# returns
+#   reply: "OK",
+#   params: [{
+#     label,
+#     prompt,
+#     type,
+#     prompt }, ... ]
+def params(view, transformation):
+  command = __make_command('params', {
+    'transformation': transformation,
+    'textselection' : __get_text_selection(view)
+    })
+  return __get_valid_response(command, view)
+
+# returns
+#   reply: "OK",
+#   result: [{
+#     valid,
+#     message }, ... ]   
+def validate(view, transformation, arguments):
+  command = __make_command('validate', {
+    'transformation': transformation,
+    'textselection' : __get_text_selection(view),
+    'arguments'     : arguments
+    })
+  return __get_valid_response(command, view)
+
+#### shee
+# returns
+#   reply: "OK",
+#   transformation,
+#   log: [{
+#     message,
+#     severity, 
+#     context: {
+#       filename,
+#       patchFile,
+#       offset,
+#       length } } ... ]
+#   files: [{
+#     filename,
+#     patchFile }, ... ]
+def xrun(view, transformation, arguments):
+  command = __make_command('xrun', {
+    'transformation': transformation,
+    'textselection' : __get_text_selection(view),
+    'arguments'     : arguments
+    })
+  return __get_valid_response(command, view)
